839c26460b934a15e784ed9307e4049484ed321b
Joy Yu Wen <wenjoyyu@gmail.com>
2025-04-12 17:01:01 +0800

    added exit game loop code

--- CSP project.py
+++ CSP project.py
@@ -9,4 +9,11 @@ while True:
     #
     #
     pygame.display.update()
-    
+
+#End game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
