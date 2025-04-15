3ba8414706f16ffea54edb44ff6e6ad88353d827
Joy Yu Wen <wenjoyyu@gmail.com>
2025-04-15 18:21:40 +0800

    Display screen set-up

--- CSP project.py
+++ CSP project.py
@@ -1,19 +1,25 @@
 import pygame
-from pygame.locals import *
+#from pygame.locals import *
+from sys import exit
+
 pygame.init()
+screen = pygame.display.set_mode((800,400))
+#To show on white bar above, we'll be in fullscreen so may not be needed
+#   pygame.display.set_caption('Game_name or what have you')
+clock = pygame.time.Clock() #limit game frame rate
 
 #Game loop begins
 while True:
-    #Code
-    #More Code
-    #
-    #
-    pygame.display.update()
-
-#End game loop
-while True:
+    # to End game loop
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            exit()
+    #all our code goes here
+    #drawn elements and updates
     pygame.display.update()
+    clock.tick(60)
+
+
+
+
