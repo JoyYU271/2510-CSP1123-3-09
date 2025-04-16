1461f04f43394eab7d2796a7dd5bfcdf845ea917
Joy Yu Wen <wenjoyyu@gmail.com>
2025-04-16 19:38:12 +0800

    Setting up

--- CSP project.py
+++ CSP project.py
@@ -1,11 +1,16 @@
 import pygame, sys
-#from pygame.locals import *
+from random import randint
+
+#create class for obstacle sprite, and player sprite
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((1280,720))
 #To show on white bar above, we'll be in fullscreen so may not be needed
 #   pygame.display.set_caption('Game_name or what have you')
 clock = pygame.time.Clock() #limit game frame rate
+
+#set up
+#sprite group?
 
 #Game loop begins
 while True:
@@ -13,7 +18,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
+            sys.exit()
     #all our code goes here
     #drawn elements and updates
     pygame.display.update()
