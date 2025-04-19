726de1afa6146062fba89088f0f2c0383dba677a
Lai <LAI.JIA.HAN@student.mmu.edu.my>
2025-04-19 13:48:12 +0800

    add walking animation

--- character movement.py
+++ character movement.py
@@ -1,37 +1,53 @@
 import pygame
 import sys
+import ctypes #library for full screen
 from pygame.locals import *
 
 pygame.init()
 
+#to get real resolution
+#ctypes.windll.user32.SetProcessDPIAware() #make sure the Python program gets the actual screen resolution, not scaled one
+#screen_width = ctypes.windll.user32.GetSystemMetrics(0) #ask the real screen width in pixels
+#screen_height = ctypes.windll.user32.GetSystemMetrics(1) #ask the real screen height in pixels
 
-screen_width = 1535
-screen_height = 775
+screen_width = 1280
+screen_height = 720
 
-screen = pygame.display.set_mode((screen_width,screen_height))
-#set framerate帧率
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+
+#set framerate
 clock = pygame.time.Clock()
+FPS = 60
 
 #define background color
 BG = (255,255,255)
 
-#清空画面 only white bg 
-# for 复杂的背景就要用screen.blit(background_image, (0, 0)) 
+# only white bg 
+# for bg img  need to use 'screen.blit(background_image, (0, 0)) '
 def draw_bg():
   screen.fill(BG)
 
 class doctor(pygame.sprite.Sprite):
     def __init__(self,x,y,speed):
-        super().__init__()   #auto find the sprite 自动找到当前类的父类（pygame.sprite.Sprite）
-        self.image =  pygame.image.load('picture/images.png')
-        #得到图片的矩形区域
+        super().__init__()   #auto find the sprite（pygame.sprite.Sprite）
+        self.stand_img = pygame.image.load('picture/stand.png').convert_alpha()
+        self.image =  self.stand_img
+        #get the rectangle area of image
         self.rect = self.image.get_rect()
         #set player on screen on origin location
         self.rect.center = (x,y)
         self.speed = speed
         self.direction = 1
-        #翻转图片 flip picture
+
+        # flip picture
         self.flip = False
+        self.animation_list = []
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+    
+        for i in range(1,5):
+            doctorwalk = pygame.image.load(f'picture/walking/walk {i}.png').convert_alpha()
+            self.animation_list.append(doctorwalk)
 
     def move(self,moving_left,moving_right):
      #reset movement variables
@@ -54,28 +70,50 @@ class doctor(pygame.sprite.Sprite):
            self.rect.left = 0
        if self.rect.right > screen_width:
            self.rect.right = screen_width
+       return moving_left or moving_right
+
+    def update_animation(self,is_moving):
+        if is_moving:
+
+            #control animation speed 
+            ANIMATION_COOLDOWN = 150
+
+            #control change of animation
+            if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+                 self.update_time = pygame.time.get_ticks()
+                 self.frame_index += 1
+
+                 if self.frame_index >= len(self.animation_list):
+                    self.frame_index = 0
+
+            self.image = self.animation_list[self.frame_index]
+        else:
+            self.image = self.stand_img
+
 
     def draw(self):
         screen.blit (pygame.transform.flip(self.image,self.flip,False),self.rect)
 
 
-player = doctor(500,550,5)    #coordinate screen, speed
+player = doctor(400,500,4.5)    #coordinate screen, speed
 
 #define player action variables
 moving_left = False
 moving_right = False
 
-
+is_moving = player.move(moving_left,moving_right)
 
 run = True 
 
 while run :
-
+    
     draw_bg()
-    player.move( moving_left, moving_right )
+    is_moving = player.move(moving_left,moving_right)
+    player.update_animation(is_moving)
     player.draw()
   
     for event in pygame.event.get():
+        
 
         if event.type == pygame.QUIT:
            run = False
@@ -98,12 +136,12 @@ while run :
         
     
    
-    
-    # 更新屏幕
+
+    # uodate display of screen
     pygame.display.update()
 
-    # 控制游戏帧率
-    clock.tick(60)    
+    # control game frames
+    clock.tick(FPS)    
            
          
 pygame.quit()
