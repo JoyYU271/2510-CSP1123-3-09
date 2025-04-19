870f1c20af1001ee68dfe3bb94f968a0448ded60
Joy Yu Wen <wenjoyyu@gmail.com>
2025-04-19 23:21:41 +0800

    Continued the tut

--- CSP project.py
+++ CSP project.py
@@ -11,7 +11,10 @@ font_handwriting = pygame.font.Font('Projects/Python game/Morning Bright.otf')
 
 fire_surface = pygame.image.load('Projects/Python game/pfp.png').convert_alpha()
 ruins_surface = pygame.image.load('Projects/Python game/Ruins.png').convert()
+
 surface_text = font_handwriting.render('My Game', True, 'Black')
+text_rect = surface_text.get_rect(center = (160, 200))
+
 collision_surface = pygame.Surface((50,25))
 collision_surface.fill('Purple')
 
@@ -36,13 +39,18 @@ while True:
     fire_rectg.bottom -= 1  #so weird, .bottom works, but .midbottom gets a tuple, int error??
     screen.blit(fire_surface,fire_rectg)
    # print(ruins_rectg.midbottom)
-    screen.blit(surface_text,(80,200))
-    
-    if fire_rectg.colliderect(collision_rectg): #I think because it calls it here, I don't have to worry about the code not being in the game loop
-        print('collision')
+    pygame.draw.rect(screen, 'Purple', text_rect,8) #draw part of pygame literally draws the rect, not stamp it or anything
+    #hmm, to settle the probelm of the empty middle, it's either print twice, or increase the width until the space get's blocked out
 
-    #was thinking if I should skip trying out collision since I don't have another image for this... until I remembered I can make rectangles
-    #create rectangle but don't display, see if collision will still detect
+
+    screen.blit(surface_text,text_rect)
+    
+    #if fire_rectg.colliderect(collision_rectg): #I think because it calls it here, I don't have to worry about the code not being in the game loop
+        #print('collision')
+
+    # was thinking if I should skip trying out collision since I don't have another image for this... until I remembered I can make rectangles
+    # create rectangle but don't display, see if collision will still detect
+    # It works :))
 
     pygame.display.update()
     clock.tick(60)
